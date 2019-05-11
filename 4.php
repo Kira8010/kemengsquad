7224e4c4ab87fbde899f8dca40c6257edf6b9679
Kira8010 <46552056+Kira8010@users.noreply.github.com>
2019-05-11 14:10:35 +0700

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,47 @@
+<?php
+    $nama = array( 
+  "P" => array ("P" => "P========P"),
+  "R1" => array ("R1" => "=R======R="),
+  "O" => array ("O" => "==O====O=="),
+  "G" => array ("G" => "===G==G==="),
+  "R2" => array ("R2" => "====RR===="),
+  "A" => array ("A" => "====AA===="),
+  "M1" => array ("M1" => "===M==M==="),
+  "M" => array ("M" => "==M====M=="),
+  "E" => array ("E" => "=E======E="),
+  "R" => array ("R" => "R========R")
+  
+ );
+ 
+  // Untuk Doni
+  echo $nama['P']['P'] . "<br />" ;
+  
+  // Untuk Didi
+  echo $nama['R1']['R1'] . "<br />" ;
+  
+  // Untuk Didi
+  echo $nama['O']['O'] . "<br />" ;
+  
+  // Untuk Didi
+  echo $nama['G']['G'] . "<br />" ;
+
+  // Untuk Didi
+ echo $nama['R2']['R2'] . "<br />" ;
+
+ // Untuk Doni
+ echo $nama['A']['A'] . "<br />" ;
+  
+ // Untuk Didi
+ echo $nama['M1']['M1'] . "<br />" ;
+ 
+ // Untuk Didi
+ echo $nama['M']['M'] . "<br />" ;
+ 
+ // Untuk Didi
+ echo $nama['E']['E'] . "<br />" ;
+
+ // Untuk Didi
+echo $nama['R']['R'] . "<br />" ;
+  
+  
+?>
